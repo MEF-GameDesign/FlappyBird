5c837dab4a977b85e2fe9f5d5220a4a522887ea1
Ozgun Balaban <ozgunbalaban@yahoo.com>
2020-03-04 03:01:13 +0300

    1st Update

---
+++ FlappyBird/Assets/Scripts/Flappy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flappy : MonoBehaviour
+{
+    public GameManager GameManager;
+    [SerializeField] public float velocity = 2;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Jump
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameManager.GameOver();
+    }
+}
